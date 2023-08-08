84293dc31f2b8c34aec71873dd03049b264cdd15
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-08-08 11:03:14 -0700

    React Intro

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import logo from './logo.jpg'
+function App() {
+  return (
+	  <div className="App">
+	  	<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+		  	<h1>School dashboard</h1>
+	  	</header>
+	  	<body className="App-body">
+	  		<p>Login to access the full dashboard</p>
+	  		<form action="#">
+  				<label for="Ename">Email: </label>
+  				<input type="text" id="Email" name="Email">
+ 				<label for="Password">Password: </label>
+  				<input type="text" id="Password" name="Password">
+ 				<input type="submit" value="OK">
+			</form> 
+	  	</body>
+	  	<footer className="App-footer">
+	  		<p>Copyright 2020 - holberton School</p>
+		</footer>
+	</div>
+  );
+}
+
+export default App;
