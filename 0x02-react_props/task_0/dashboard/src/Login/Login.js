cbb363070fa7969ebfc7a44068f09b66fc2cccba
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-08-13 11:08:12 -0700

    React Props

---
+++ 0x02-react_props/task_0/dashboard/src/Login/Login.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './Login.css'
+
+export default class App extends Component {
+	render() {
+		return (
+			<div className="App">
+					<body className="App-body">
+						<p>Login to access the full dashboard</p>
+	  					<form action="#">
+  							<label for="Ename">Email: </label>
+  							<input type="text" id="Email" name="Email"/>
+ 							<label for="Password">Password: </label>
+  							<input type="text" id="Password" name="Password"/>
+ 							<input type="submit" value="OK"/>
+						</form> 
+					</body>
+			</div>
+			</React.Fragment>
+		)
+	}
+}
