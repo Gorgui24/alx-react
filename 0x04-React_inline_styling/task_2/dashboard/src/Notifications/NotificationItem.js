ad60f3053868c7b6c329afc51ce1706586937a0b
Gorgui24 <fayesouleymanegorgui@gmail.com>
2023-08-16 04:49:55 -0700

    React Inline Styling

---
+++ 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,55 @@
+import React, { PureComponent } from 'react'
+import propTypes from 'prop-types'
+
+
+class NotificationItem extends PureComponent {
+	render() {
+		if (type === 'urgent') {
+		return (
+			<li onCLick={() => { markAsRead(id) }}
+				data-notification-type={type}
+				dangerouslySetInnerHTML={html}
+				className={css(itemStyles.urgent)}
+			>
+				{value}
+			</li>
+		)
+	}
+	return (
+		<li onCLick={() => { markAsRead(id) }}
+			data-notification-type={type}
+			dangerouslySetInnerHTML={html}
+			className={css(itemStyles.default)}
+		>
+			{value}
+		</li>
+	)
+	}
+}
+const itemStyles = StyleSheet.create({
+	urgent: {
+		color: 'red'
+	},
+
+	default: {
+		color: 'blue'
+	}
+})
+
+NotificationItem.propTypes = {
+	type: propTypes.string,
+	value: propTypes.string,
+	html: propTypes.shape({
+		__html: propTypes.string,
+	}),
+	markAsRead: propTypes.func,
+	id: propTypes.number,
+}
+
+NotificationItem.defaultProps = {
+	type: 'default',
+	markAsRead: () => {},
+	id: 0,
+}
+
+export default NotificationItem
